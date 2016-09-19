a1ef87e1f2ea81239d565b45656f01459fd8056a
sarazhang5 <1733235185@qq.com>
2016-09-19 14:50:44 +0800

    change speed1

--- 物体匀速运动.js
+++ 物体匀速运动.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2016-09-07 20:55:50
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-09-19 14:38:49
+* @Last Modified time: 2016-09-19 14:46:33
 */
 //留言
 //ie8不能运行，火狐浏览器向下有问题，谷歌浏览器正常
@@ -10,6 +10,7 @@ window.onload=function(){
 var oBtn=document.getElementById('button');
 var oImg=document.getElementById('image');
 var timer=null;
+var iSpeed=10;
 oBtn.onclick=function(){
     clearInterval(timer);
     timer=setInterval(function(){
@@ -17,7 +18,7 @@ oBtn.onclick=function(){
             alert('到达终点');
             clearInterval(timer);
         }else{
-            oImg.style.left=oImg.offsetLeft+10+'px';
+            oImg.style.left=oImg.offsetLeft+iSpeed+'px';
         }
     },100);
 
