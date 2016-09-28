f53551a3474a1806d41055156c8096b768c96e7a
sarazhang5 <1733235185@qq.com>
2016-09-28 10:20:09 +0800

    codeeveryday

--- 自定义属性实现数字递增.js
+++ 自定义属性实现数字递增.js
@@ -2,10 +2,8 @@
 * @Author: Marte
 * @Date:   2016-09-07 20:55:50
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-09-28 10:10:09
+* @Last Modified time: 2016-09-28 10:18:47
 */
-//留言
-//ie8不能运行，火狐浏览器向下有问题，谷歌浏览器正常
 window.onload=function(){
   oBtn=document.getElementsByTagName('input');
   var arr=['1','2','3','4','5'];
