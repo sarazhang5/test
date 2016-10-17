2c7153ed1d01bf4cc993d5477452a33a8add5d7e
sarazhang5 <1733235185@qq.com>
2016-10-17 10:02:38 +0800

    codeeveryday

---
+++ js中的作用域.js
@@ -0,0 +1,60 @@
+/* 
+* @Author: Marte
+* @Date:   2016-09-07 20:55:50
+* @Last Modified by:   Marte
+* @Last Modified time: 2016-10-17 09:56:22
+*/
+
+window.onload=function(){
+    //1
+  /*.alert(a);//undefined
+  var a=1;*/
+    //2
+  /*alert(a);// a is not defined
+  a=1;*/
+    //3
+  /*alert(a);//undefined
+  var a=1;
+  alert(a)//1
+  function fn1(){alert(2);}//没有弹出*/
+     //4
+ /*alert(a); //function a(){alert(4);}
+  var a=1;
+  alert(a);//1
+  function a(){alert(2);}
+  alert(a);//1
+  var a=3;
+  alert(a);//3
+  function a(){alert(4);}
+  alert(a);//3
+  alert(typeof a);//number
+  a();// a is not a function*/
+
+    //5
+  /*var a=1;
+  function fn1(){
+    alert(a);
+    var a=2;
+  }
+  fn1();//undefined
+  alert(a)//1 */
+    //6
+/*var a=1;
+  function fn1(){
+    alert(a);//1
+     a=2;
+  }
+  fn1();
+  alert(a)//2 */
+    //7
+  var a=1;
+  function fn1(a){
+    alert(a);
+     a=2;
+  }
+  fn1();
+  alert(a)
+
+
+}
+    
